d90a7d6d43ee863295f4d802ae7e37917f0db58f
César Costa <costa.cesar.a@hotmail.com>
2017-02-14 13:33:35 -0300

    Webpack updated to v2; devDependencies updated.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 var HTMLWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
 var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
   template: __dirname + '/src/index.html',
   filename: 'index.html',
@@ -6,22 +8,22 @@ var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
 });
 
 module.exports = {
-  entry: './src/app.jsx',
+  entry: path.resolve(__dirname, 'src/app.jsx'),
   resolve: {
-    root: __dirname,
-    extensions: ['', '.js', '.jsx'],
+    extensions: ['.js', '.jsx'],
     alias: {
     }
   },
   module: {
-    loaders: [
-      {test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader', query: {presets: ["es2015", "react"]}}
+    rules: [
+      {test: /\.jsx?$/, include: path.resolve(__dirname, 'src'), loader: 'babel-loader', options: {presets: ['es2015', 'react']}}
     ]
   },
   output: {
-    filename: 'app.bundle.js',
-    path: __dirname
+    path: path.resolve(__dirname),
+    filename: 'app.bundle.js'
   },
   plugins: [HTMLWebpackPluginConfig],
-  devtool: 'cheap-module-eval-source-map'
+  devtool: 'cheap-module-eval-source-map',
+  target: 'web'
 }
